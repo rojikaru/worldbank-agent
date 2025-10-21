004fddf2ef983c2fa625378c927d2fe33a5cd490
Alex Ivanov <oleksii120@gmail.com>
2025-10-21 14:14:52 +0300

    feat: set up langgraph server

---
+++ apps/agents/src/stratbot_agent/graph.ts
@@ -0,0 +1,74 @@
+import { AIMessage } from "@langchain/core/messages";
+import { RunnableConfig } from "@langchain/core/runnables";
+import {
+  MessagesAnnotation,
+  StateGraph,
+  START,
+  END,
+} from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+
+import { ConfigurationSchema, ensureConfiguration } from "./configuration";
+import { TOOLS } from "./tools";
+import { loadChatModel } from "./utils";
+
+/** Call the LLM powering our agent. **/
+async function callModel(
+  state: typeof MessagesAnnotation.State,
+  config: RunnableConfig
+): Promise<typeof MessagesAnnotation.Update> {
+  const configuration = ensureConfiguration(config);
+
+  // Feel free to customize the prompt, model, and other logic!
+  const model = await loadChatModel(configuration.model).then((m) =>
+    m.bindTools(TOOLS)
+  );
+
+  const response = await model.invoke([
+    {
+      role: "system",
+      content: configuration.systemPromptTemplate.replace(
+        "{system_time}",
+        new Date().toISOString()
+      ),
+    },
+    ...state.messages,
+  ]);
+
+  // We return a list, because this will get added to the existing list
+  return { messages: [response] };
+}
+
+// Define the function that determines whether to continue or not
+function routeModelOutput(state: typeof MessagesAnnotation.State): string {
+  const lastMessage = state.messages.at(-1);
+  if (!AIMessage.isInstance(lastMessage)) {
+    return END;
+  }
+
+  const toolCalls = lastMessage.tool_calls ?? [];
+
+  // If the LLM is invoking tools, route there.
+  // Otherwise end the graph.
+  return toolCalls.length > 0 ? "tools" : END;
+}
+
+// Define a new graph. We use the prebuilt MessagesAnnotation to define state:
+// https://langchain-ai.github.io/langgraphjs/concepts/low_level/#messagesannotation
+const workflow = new StateGraph(MessagesAnnotation, ConfigurationSchema)
+  // Define the two nodes we will cycle between
+  .addNode("callModel", callModel)
+  .addNode("tools", new ToolNode(TOOLS))
+  // Set the entrypoint as `callModel`
+  // This means that this node is the first one called
+  .addEdge(START, "callModel")
+  // Decide where to go after calling the model, if needed
+  .addConditionalEdges("callModel", routeModelOutput)
+  // Pass tool outputs back to the model
+  .addEdge("tools", "callModel");
+
+// This compiles it into a graph you can invoke and deploy.
+export const graph = workflow.compile({
+  interruptBefore: [], // if you want to update the state before calling the tools
+  interruptAfter: [],
+});
