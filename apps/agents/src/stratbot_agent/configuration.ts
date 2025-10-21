7a34025ad01f5fff8acf0cda53236aaab9f9dce1
Alex Ivanov <oleksii120@gmail.com>
2025-10-21 14:20:08 +0300

    chore: define supported models

--- apps/agents/src/stratbot_agent/configuration.ts
+++ apps/agents/src/stratbot_agent/configuration.ts
@@ -7,22 +7,50 @@ import { SYSTEM_PROMPT_TEMPLATE } from "./prompts";
 import { RunnableConfig } from "@langchain/core/runnables";
 import process from "node:process";
 
-// Define which models are available based on environment variables
+type ModelRequirements = {
+  requirements: {
+    name: string;
+    value: string | undefined;
+  }[];
+};
+
+// Define which models are available and their requirements
 const defaultModels = {
-  "claude-haiku-4-5-20251001":
-    typeof process.env.ANTHROPIC_API_KEY === "string",
-  "gpt-4-mini": typeof process.env.OPENAI_API_KEY === "string",
-} as const;
+  "claude-haiku-4-5-20251001": {
+    requirements: [
+      {
+        name: "ANTHROPIC_API_KEY",
+        value: process.env.ANTHROPIC_API_KEY,
+      },
+    ],
+  },
+  "gpt-4.1-mini": {
+    requirements: [
+      {
+        name: "OPENAI_API_KEY",
+        value: process.env.OPENAI_API_KEY,
+      },
+    ],
+  },
+} as const satisfies Record<string, ModelRequirements>;
+
+type RegisteredModels = keyof typeof defaultModels;
 
 // Find the first available model
 const defaultModel = Object.entries(defaultModels)
-  .find(([, available]) => available)
-  ?.at(0) as keyof typeof defaultModels;
+  .find(([, { requirements }]) =>
+    requirements.every((req) => req.value !== undefined)
+  )
+  ?.at(0) as RegisteredModels;
 
 // Assert that we have at least one model available
 if (defaultModel === undefined) {
   throw new Error(
-    "No language model API keys found in environment variables. Please set ANTHROPIC_API_KEY or OPENAI_API_KEY."
+    "No language model API keys found in environment variables. " +
+      "Please set any of the following environment variables: " +
+      Object.values(defaultModels)
+        .flatMap(({ requirements }) => requirements.map((req) => req.name))
+        .join(", ")
   );
 }
 
